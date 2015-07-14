8d72c2578261a14c48e139b245c4b8513b3bf7dc
Sergey Serebryakov <sserebryakov@twitter.com>
2015-07-13 20:39:17 -0700

    Sort of working solution

--- simulatedAnnealing.js
+++ simulatedAnnealing.js
@@ -6,29 +6,88 @@ var items = [ // allowed multiple of each
   {name:'radio',    weight:6, value:30}
 ];
 
+var MAX_TRIES = 100;
+
 function generateRandomSolution(){
+  var solution = [];
+  var totalWeight = 0;
+
+  while (true) {
+    // maybe enough already
+    if (Math.random() < 0.1) break;
+
+    // try to choose an item that fits
+    for (var t = 0; t < MAX_TRIES; t++) {
+      var index = randomIndex(items);
+      if (totalWeight + items[index].weight <= knapsack.maxWeight) {
+        totalWeight += items[index].weight;
+        solution.push(items[index]);
+        break;
+      }
+    }
+  }
 
   return solution; // array of items, must be <= maxWeight
 };
 
 function generateNeighboringSolution(oldSolution){
-  // add, swap, or remove item randomly
+  var chooseStep = Math.floor(Math.random() * 2);
 
+  var solution = [];
+  for (var i in oldSolution) solution.push(oldSolution[i]);
+
+  if (chooseStep == 0) {
+    var totalWeight = 0;
+    for (var i in oldSolution) totalWeight += oldSolution[i].weight;
+
+    // try to choose an item that fits
+    for (var t = 0; t < MAX_TRIES; t++) {
+      var index = randomIndex(items);
+      if (totalWeight + items[index].weight <= knapsack.maxWeight) {
+        solution.push(items[index]);
+        return solution;
+      }
+    }
+  }
+
+  // either chooseStep == 1, or we failed to add
+  var indexToRemove = randomIndex(oldSolution);
+  solution = solution.splice(indexToRemove, 1);
   return solution; // array of items, must be <= maxWeight
 }
 
 function calculateCost(solution){
-
-  return cost; // sum of values of items
+  var sum = 0;
+  for (var i in solution) sum += solution[i].value;
+  return sum; // sum of values of items
 }
 
 function acceptance_probability(old_cost, new_cost, temperature){
   return Math.pow(Math.E, (new_cost - old_cost)/temperature); // probability to jump
 }
 
-function simulateAnnealing(){
+function simulateAnnealing() {
+  function anneal(solution) {
+    var oldCost = calculateCost(solution);
+    var t = 1.0;
+    var minT = 0.00001;
+    var alpha = 0.9;
 
-  return solution; // array of items, must be <= maxWeight
+    while (t > minT) {
+      for (var i = 1; i <= 100; i++) {
+        var newSolution = generateNeighboringSolution(solution);
+        var newCost = calculateCost(newSolution);
+        var ap = acceptance_probability(oldCost, newCost, t);
+        if (Math.random() < ap) {
+          solution = newSolution;
+          oldCost = newCost;
+        }
+      }
+      t *= alpha;
+    }
+    return solution;
+  }
+  return anneal(generateRandomSolution()); // array of items, must be <= maxWeight
 };
 
 ///////////////////////////////////
